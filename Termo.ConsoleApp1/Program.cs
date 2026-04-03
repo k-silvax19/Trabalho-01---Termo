cc5cc2e2bc758a68f6794905cd04002ade5fa776
Kauan Silva <kauanmacedo338@gmail.com>
2026-04-03 15:28:23 -0300

    Adionado esquema de foreach (esquema de laço para loop com string, array e etc), salva as palavras escritas anteriormente.

--- Termo.ConsoleApp1/Program.cs
+++ Termo.ConsoleApp1/Program.cs
@@ -10,9 +10,11 @@ public static class Program
         while (true == true)
         {
             string[] ultimasPalavras = new string[0];
-            string palavraDigitada = "";
+            string palavraDigitada;
             string palavraParaJogar = "".SortWord();
+            int tentativas = 5;
 
+            // cabecalho    
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("╔════════════════════════════════════╗");
@@ -32,68 +34,92 @@ public static class Program
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine(" ");
 
-            while (palavraDigitada != palavraParaJogar && palavraDigitada != "SAIR")
+            // logica do jogo
+            while (true)
             {
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Digite uma palavra: ");
                 palavraDigitada = Console.ReadLine().ToUpper();
 
-
-                if (palavraDigitada.Length != 0)
+                if (string.IsNullOrWhiteSpace(palavraDigitada))
                 {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("últimas palavras digitadas: " + string.Join(", ", ultimasPalavras) + "\n");
+                    Console.WriteLine("Digite algo válido!");
+                    continue;
                 }
-
-
-                if (palavraDigitada == "SAIR")
+                else if (palavraDigitada == "SAIR")
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Obrigado por jogar!");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.ReadLine();
                     break;
                 }
-
-                if (ultimasPalavras.Contains(palavraDigitada))
+                else if (ultimasPalavras.Contains(palavraDigitada))
                 {
                     Console.WriteLine("Você já digitou essa palavra!");
                     continue;
                 }
-
-                if (palavraDigitada.Length > palavraParaJogar.Length)
+                else if (palavraDigitada.Length != palavraParaJogar.Length)
                 {
-                    Console.WriteLine("A palavra digitada é maior que a palavra para jogar!");
+                    Console.WriteLine("A palavra deve ter o tamanho correto!");
                     continue;
                 }
 
                 ultimasPalavras = ultimasPalavras.Append(palavraDigitada).ToArray();
 
-                //laco de repetição para verificar cada letra da palavra digitada e comparar com a palavra para jogar
-                for (int i = 0; i < palavraDigitada.Length; i++)
+                Console.WriteLine(" ");
+
+                //foreach (esquema de laço para loop com string, array e etc), ele funciona aqui para imprimir os acertos/erros e imprimir atras as letras anteriores
+                foreach (string palavra in ultimasPalavras)
                 {
-                    //letra na posição correta
-                    if (palavraDigitada[i] == palavraParaJogar[i])
+                    for (int contador = 0; contador < palavraParaJogar.Length; contador++)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(palavraParaJogar[i]);
+                        if (palavra[contador] == palavraParaJogar[contador])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(palavra[contador]);
+                        }
+                        else if (palavraParaJogar.Contains(palavra[contador]))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(palavra[contador]);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write(palavra[contador]);
+                        }
                     }
 
-                    //letra existe na palavra mas não está na posição correta
-                    else if (palavraParaJogar.Contains(palavraDigitada[i]))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write(palavraDigitada[i]);
-                    }
-
-                    //letra não existe na palavra
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write(palavraDigitada[i]);
-                    }
+                    Console.WriteLine("");
                 }
+
+                // verificação de tentativas
+                if (palavraDigitada == palavraParaJogar)
+                {
+                    Console.WriteLine($"Parabéns você ganhou! A palavra era: {palavraParaJogar}");
+                    Console.ReadLine();
+                    break;
+                }
+
+                tentativas--;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Tentativas restantes: {tentativas}");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // perdeu
+                if (tentativas == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Você perdeu! A palavra era: {palavraParaJogar}");
+                    Console.ReadLine();
+                    break;
+                }
+
                 Console.WriteLine();
             }
         }
-
     }
 
     public static string SortWord(this string word)
@@ -131,7 +157,7 @@ public static class Program
                 "TARDE"
             ];
         Random random = new Random();
-        int index = random.Next(Lista.Length);
-        return Lista[index];
+        int dicionario = random.Next(Lista.Length);
+        return Lista[dicionario];
     }
 }
